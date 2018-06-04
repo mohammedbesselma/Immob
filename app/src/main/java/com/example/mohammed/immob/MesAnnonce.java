950ae0be60164a8a55f08d095454d1bd6689c39a
Mohammed <mohammedbesselma@gmail.com>
2018-06-04 05:32:16 +0200

    lastap

---
+++ app/src/main/java/com/example/mohammed/immob/MesAnnonce.java
@@ -0,0 +1,34 @@
+package com.example.mohammed.immob;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MesAnnonce extends Fragment {
+    ListView mesannonce;
+
+
+    public MesAnnonce() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mes_annonce, container, false);
+
+
+
+        return view;
+    }
+
+}
