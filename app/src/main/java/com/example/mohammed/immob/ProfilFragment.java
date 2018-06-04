a8f94f244a81c679921ea1ac60e04927ed6c7cc4
Mohammed <mohammedbesselma@gmail.com>
2018-06-05 00:11:15 +0200

    lastap

--- app/src/main/java/com/example/mohammed/immob/ProfilFragment.java
+++ app/src/main/java/com/example/mohammed/immob/ProfilFragment.java
@@ -42,8 +42,7 @@ public class ProfilFragment extends Fragment {
         conecter1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myintent = new Intent(getActivity(),LoginActivity.class);
-                getActivity().startActivity(myintent);
+                
             }
         });
         tomes.setOnClickListener(new View.OnClickListener() {
